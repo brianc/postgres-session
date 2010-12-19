5bb1a05f120269ef5a318ecb94f59d603cf9f743
brianc <brian.m.carlson@gmail.com>
2010-12-19 17:02:09 -0600

    some cleanup

--- test/sanity-tests.js
+++ test/sanity-tests.js
@@ -8,13 +8,16 @@ var Store = require('connect/middleware/session/store');
 var connectionString = process.argv[2].replace("--con=","");
 var tableName = "test_connect_session";
 
+var log = function() {
+  console.log.apply(console, arguments);
+}
+
 var testSetup = function(callback) {
   pg.defaults.poolSize = 1;
-  console.log('connecting to %s', connectionString);
+  log('connecting to %s', connectionString);
   pg.connect(connectionString, should.call(function(err, client) {
-    console.log("connected");
+    log("connected");
     assert.isNull(err)
-    console.log("no connect error");
     client.query('DROP TABLE ' + tableName, function(err) {
       //disregard the error
       client.query("CREATE TABLE " + tableName + " (key varchar(64), data varchar(255))", callback)
@@ -32,26 +35,29 @@ sys.inherits(PostgresStore, Store)
 var p = PostgresStore.prototype;
 
 p.get = function(hash, callback) {
-  console.log("called get");
+  log("called get");
   pg.connect(this.connectionString, function(err, client) {
     if(err) {
       return callback(err);
     }
-    client.query("SELECT data FROM" + tablename + "WHERE key = $1", [hash], function(err, result) {
-      console.log("recieved result %j", result);
-      var item = JSON.parse(result.rows[0].data||"{}");
-      console.log(item);
+    client.query("SELECT data FROM " + tableName + " WHERE key = $1", [hash], function(err, result) {
+      log("recieved result %j", result);
+
+      if(!err) {
+        var item = JSON.parse(result.rows[0].data||"{}");
+        log(item);
+      }
       return callback(err, item)
     })
   })
 }
 
 p.set = function(hash, data, callback) {
-  console.log("setting %s %j", hash, data)
+  log("setting %s %j", hash, data)
   pg.connect(this.connectionString, function(err, client) {
-    console.log("Connected");
+    log("Connected");
     if(err) {
-      console.log("Error");
+      log("Error");
       return callback(err);
     }
     client.query("INSERT INTO " + tableName +"(key, data) VALUES($1, $2)",[hash, JSON.stringify(data)], callback)
@@ -59,48 +65,55 @@ p.set = function(hash, data, callback) {
 }
 var store = new PostgresStore({connectionString: connectionString});
 
+var testServer = connect.createServer(
+  connect.cookieDecoder(),
+  connect.session({ store: store}),
+  function(req, res) {
+    req.session.hits = (req.session.hits || 0)+1
+    res.writeHead(200, {"Content-Type": "text/html"});
+    res.end(req.session.hits + " hits")
+    testServer.close();
+  }
+)
 
 var startTestServer = function(callback) {
-  var server = connect.createServer(
-    connect.cookieDecoder(),
-    connect.session({ store: store}),
-    function(req, res) {
-      res.writeHead(200, {"Content-Type": "text/html"});
-      res.end("BOOM")
-      server.close();
-    }
-  );
-  server.listen(8081, callback)
+  testServer.listen(8081, callback)
 }
 
-var request = should.call(function(method, url, callback) {
+var request = should.call(function(method, url, headers, callback) {
   startTestServer(function() {
     var testClient = http.createClient(8081, 'localhost');
-    var request = testClient.request(method, url);
+    if(typeof headers == 'function') {
+      callback = headers;
+      headers = {};
+    }
+    var request = testClient.request(method, url, headers);
     request.end();
     request.on('response', should.call(callback))
   })
 })
 
-var get = function(url, callback) {
+var get = function(url, headers, callback) {
   test('getting ' + url, should.call(function() {
-    request('GET', url, callback)
+    request('GET', url, headers, callback)
   }))
 }
 
-var post = function(url, callback) {
+var post = function(url, headers, callback) {
   test('posting to ' + url, function() {
-    request('POST', url, callback)
+    request('POST', url, headers, callback)
   })
 }
 
 
 testSetup(function() {
-  console.log("starting tests")
+  var headers = {};
+
   get('/', function(response) {
-    console.log("Response was called");
+    headers["cookie"] = response.headers["set-cookie"][0].split(';')[0];
     response.statusCode.should.equal(200)
     pg.end();
   })
+
 });
 
